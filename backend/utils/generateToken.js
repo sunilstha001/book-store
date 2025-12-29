b580e11c249e4f9f77c1cb9947ca90de049b0415
Sunil Shrestha <sthasunil664@gmail.com>
2025-12-29 15:27:16 +0530

    edited in cookies while creating token

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,8 +9,8 @@ const generateTokenAndSetCookie = (res, userId) => {
   // Save the token in an HTTP-Only cookie
   res.cookie('jwt_token', token, {
     httpOnly: true, // Prevents JavaScript from reading the cookie
-    secure: false, // Set to 'true' in production (when using HTTPS)
-    sameSite: 'strict', // Helps prevent CSRF attacks
+    secure: true, // Set to 'true' in production (when using HTTPS)
+    sameSite: "none", // Helps prevent CSRF attacks
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
